16546a13acbc61c0fab9124321103341aaa52a8e
raddle60 <raddle60@gmail.com>
2011-12-07 13:24:35 +0000

    分三种颜色

--- src/main/java/com/raddle/swing/hosts/switcher/pane/HostTableCellRenderer.java
+++ src/main/java/com/raddle/swing/hosts/switcher/pane/HostTableCellRenderer.java
@@ -14,8 +14,12 @@ public class HostTableCellRenderer extends DefaultTableCellRenderer {
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Component rendererComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
         HostWrapper wrapper = (HostWrapper) table.getValueAt(row, 0);
-        if (wrapper.isActive()) {
-            rendererComponent.setForeground(Color.BLUE);
+        if (wrapper.isExist()) {
+            if (wrapper.isActive()) {
+                rendererComponent.setForeground(Color.BLUE);
+            } else {
+                rendererComponent.setForeground(Color.RED);
+            }
         } else {
             rendererComponent.setForeground(Color.GRAY);
         }
